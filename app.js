a4e102ee8139562cd985527c92a5c89f42ca6730
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-22 18:06:33 +0530

    chore: Add API endpoint for updating a specific tour

--- app.js
+++ app.js
@@ -57,6 +57,21 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > toursData.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: `Updated Tours Here`,
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
