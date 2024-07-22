8e85d54f06b2f55d67bc47295b67b7058752e1d8
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-22 16:42:56 +0530

    feat: Update root route to return JSON response

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from server Ankur Halder',
-    app: 'Natours',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello from server Ankur Halder',
+//     app: 'Natours',
+//   });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('posting!');
+// app.post('/', (req, res) => {
+//   res.status(200).send('posting!');
+// });
+const tours = fs.readFileSync(
+  `${__dirname}/dev-data/data/tours-simple.json`,
+  'utf-8'
+);
+toursData = JSON.parse(tours);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: toursData.length,
+    data: { toursData },
+  });
 });
 
 const port = 3000;
