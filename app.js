8c36cf23a1703d19d5744ecbcb51c6e3b0a9e1a4
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-22 16:06:29 +0530

    chore: Update root route to return JSON response

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Ankur From the server side');
+  res.status(200).json({
+    message: 'Hello from server Ankur Halder',
+    app: 'Natours',
+  });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send('posting!');
 });
 
 const port = 3000;
