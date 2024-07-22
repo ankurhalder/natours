50adb29a95643af16a8a7566c92e398ce6b00198
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-22 17:12:29 +0530

    feat: Add API endpoint for creating new tours

--- app.js
+++ app.js
@@ -2,21 +2,14 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     message: 'Hello from server Ankur Halder',
-//     app: 'Natours',
-//   });
-// });
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//   res.status(200).send('posting!');
-// });
 const tours = fs.readFileSync(
   `${__dirname}/dev-data/data/tours-simple.json`,
   'utf-8'
 );
-toursData = JSON.parse(tours);
+const toursData = JSON.parse(tours);
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -25,7 +18,31 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newId = toursData[toursData.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  toursData.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursData, null, 2),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'fail',
+          message: 'Internal Server Error',
+        });
+      }
+      res.status(201).json({
+        status: 'success',
+        results: toursData.length,
+        data: { tour: newTour },
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App is running on the port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
