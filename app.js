23bc1eb9a88d921c2bf9387d4b8a82cbb2e677f3
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-23 12:31:53 +0530

    chore: Update app.js and package.json for server configuration

--- app.js
+++ app.js
@@ -25,8 +25,4 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
-// Server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
+module.exports = app;
