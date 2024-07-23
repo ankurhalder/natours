19429310c84b65a315cd4fb68d5ce5f4c42eeec7
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-23 21:16:51 +0530

    chore: Update npm dependency for mongoose to version 8.5.1

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+/* eslint-disable no-console */
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({
   path: "./config.env",
 });
+
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.MONGO_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("DB Connection Successful!");
+  })
+  .catch((err) => {
+    console.error("DB Connection Error: ", err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`App is running on port ${port}`);
 });
