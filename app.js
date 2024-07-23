2e4d895bf07a4809cbcdd7e5f53fd39edfb65523
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-23 14:36:40 +0530

    chore: Add conditional check for NODE_ENV in app.js

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // middlewares
-app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
