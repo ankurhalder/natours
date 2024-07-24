edee700e1ccd7c4a05e4586ab1a0c356fceb9c08
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-24 15:45:42 +0530

    chore: Refactor updateTour and deleteTour functions in tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,11 +50,7 @@ exports.createTour = async (req, res) => {
 
 exports.updateTour = async (req, res) => {
   try {
-    // const newTour = await Tour.create(req.body);
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const tour = await Tour.findByIdAndUpdate(req.params.id);
     res.status(201).json({
       status: "success",
       data: { tour: tour },
@@ -67,9 +63,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 };
