3cd990d2be56ea03a172c0abca2a102a074b5f1d
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-24 15:09:13 +0530

    feat: Add async/await to createTour function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-// const Tour = require("../models/tourModel");
+const Tour = require("../models/tourModel");
 
 // const tours = fs.readFileSync(
 //   `${__dirname}/../dev-data/data/tours-simple.json`,
@@ -16,15 +16,15 @@
 //   next();
 // };
 
-exports.checkBody = (req, res, next) => {
-  if (req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Missing name or Price",
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: "fail",
+//       message: "Missing name or Price",
+//     });
+//   }
+//   next();
+// };
 
 // const toursData = JSON.parse(tours);
 exports.getAllTours = (req, res) => {
@@ -45,12 +45,19 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: "success",
-    // results: toursData.length,
-    // data: { tour: newTour },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
