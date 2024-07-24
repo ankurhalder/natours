0d68336b47d121c30a464c032b532b8dff17d744
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-24 19:48:26 +0530

    feat: Add endpoint for getting tour statistics

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
+router.route("/tour-stats").get(tourController.getTourStats);
 
 router
   .route("/")
