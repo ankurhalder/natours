ca95b9ba60ee18a62815d4fbd562caf6d430c153
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-24 16:05:06 +0530

    feat: Add trim option to tour name and description fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,54 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A Tour must have a name"],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A Tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A Tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A Tour must have a difficulty"],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, "A Tour must have a price"],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A Tour must have a description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A Tour must have a cover Image"],
+  },
+  images: [String],
+  cretedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
