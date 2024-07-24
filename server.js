9ac73e9d7eb9f7241479d1a42ffcb911b818f318
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-24 13:28:23 +0530

    chore: Disable prettier/prettier rule and add Tour model in server.js

--- server.js
+++ server.js
@@ -17,6 +17,24 @@ mongoose.connect(DB).then(() => {
   console.log("DB Connection Successful!");
 });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A Tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A Tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
