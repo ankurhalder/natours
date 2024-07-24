aba92ab0edfa18574e4a5fb486e133cca7c27a41
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-24 18:21:38 +0530

    feat: Add pagination to getAllTours function in tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,6 +31,16 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select("-__v");
     }
+    //# 4) PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numberTours = await Tour.countDocuments();
+      if (skip >= numberTours) throw new Error("This page Does not Exist");
+    }
     //@ EXECUTE QUERY
 
     const tours = await query;
