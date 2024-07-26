331a9ab23e8e0e309719491c735078ed0bfcd383
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-26 20:37:06 +0530

    refactor: Improve error handling in errorController.js and add error middleware

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,35 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     messege: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  //# Operational trusted error: Send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      messege: err.message,
+    });
+    //# Programming or other unknown error: dont leak error details
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
