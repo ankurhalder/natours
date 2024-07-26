f6bc1c3a2c8df880f9dee8531bf96a43ad1884bf
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-26 15:00:02 +0530

    feat: Add validator package for tour name validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, "Tour must have less or equal then 40 characters"],
       minlength: [10, "Tour must have less or equal then 10 characters"],
+      validate: [validator.isAlpha, "Tour name must only contain characters"],
     },
     slug: String,
     duration: {
@@ -44,6 +46,13 @@ const tourSchema = new mongoose.Schema(
     },
     priceDiscount: {
       type: Number,
+      validate: {
+        message: "Discount price ({VALUE}) should be below regular price",
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+      },
     },
     summary: {
       type: String,
