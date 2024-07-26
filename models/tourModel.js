de5fb683d0f906c3f95aa5896b36d403d5130c73
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-26 13:54:44 +0530

    chore: Add slugify package for generating slugs in tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A Tour must have a duration"],
@@ -66,6 +68,21 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE : runs before the .save() and .create()
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+// tourSchema.pre("save", (next) => {
+//   console.log("will save document");
+//   next();
+// });
+
+// tourSchema.post("save", (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
