eccccb19f17d478adc82b993531ba75b3a87faba
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-26 19:28:48 +0530

    refactor: Improve error handling in app.js and add error middleware

--- app.js
+++ app.js
@@ -29,10 +29,21 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Cant find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Cant find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  err.status = " fail";
+  err.statusCode = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    messege: err.message,
   });
-  next();
 });
 module.exports = app;
