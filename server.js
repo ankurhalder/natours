de55b3247a54a795fe2957d4214bf668bbf41350
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-27 14:47:07 +0530

    refactor: Improve error handling in server.js and add unhandled rejection handler

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose.connect(DB).then(() => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 💥 Shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
 });
