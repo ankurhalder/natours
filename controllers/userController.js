3266255349b836bf47d57a88c36904ef7aed4047
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-28 16:30:58 +0530

    refactor: Implement login functionality in authController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,14 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    messege: "This route is not yet defined",
+const User = require("../models/userModel");
+const catchAsync = require("../utils/CatchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: { users },
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
--- models/userModel.js
+++ models/userModel.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a Password"],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -45,6 +46,13 @@ userSchema.pre("save", async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
