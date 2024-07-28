2070de1f13e6e034a35b2f7b5cb8c6f7dc9faea0
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-28 14:21:14 +0530

    refactor: Hash user password before saving in userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -31,6 +32,18 @@ const userSchema = new mongoose.Schema({
       message: "Passwords are not the same",
     },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password wit cost of 14
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete confirm password
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
