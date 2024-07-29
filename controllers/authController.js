ef9b065d49a3e3103d10007550a035a2ef42872f
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-29 15:07:16 +0530

    refactor: Implement forgot password functionality in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,3 +95,19 @@ exports.restrictTo =
     }
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // # 1) get user based on POSTed email
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+  if (!user) {
+    next(new AppError("There is no user with provided email address", 404));
+  }
+  // # 2) Generate the random reset token
+
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  // # 3) Send it to users email
+});
+exports.resetPassword = (req, res, next) => {};
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
@@ -41,6 +42,12 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: {
     type: Date,
   },
+  passwordResetToken: {
+    type: String,
+  },
+  passwordResetExpires: {
+    type: Date,
+  },
 });
 
 userSchema.pre("save", async function (next) {
@@ -71,6 +78,19 @@ userSchema.methods.changedpasswordAfter = function (JWTTimestamp) {
   }
   // False means not changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  console.log({ resetToken }, this.passwordResetToken);
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model("User", userSchema);
