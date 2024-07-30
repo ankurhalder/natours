1959ab691f49c0ae63646310452839dec684b16f
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-30 18:57:54 +0530

    refactor: Add rate limiting middleware to API routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -9,10 +10,20 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// middlewares
+//@ Global  middlewares
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  // # Try to use JSON is response in future
+  message: "Too many requests from this IP, plaese try again in an hour!",
+});
+
+app.use("/api", limiter);
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
