d277e9ebc43dc389ab8eea429c379acc4e80ea87
Ankur Halder <ankur.halder12345@gmail.com>
2024-07-30 14:26:03 +0530

    refactor: Update userController.js to include updateMe route for updating user information

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/CatchAsync");
+const AppError = require("../utils/appError");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -9,6 +18,33 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: { users },
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //#  1) Create error if user POSTs passord data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword",
+        400,
+      ),
+    );
+  }
+  //#  2) Update user document
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    results: updatedUser.length,
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,6 +15,9 @@ router.patch(
   authController.protect,
   authController.updatePassword,
 );
+
+router.patch("/updateMe", authController.protect, userController.updateMe);
+
 router
   .route("/")
   .get(userController.getAllUsers)
